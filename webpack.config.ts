bc48a3f71255d5e442b2a87f145b4ddf8a864015
Pavel <pauledelweissontop@gmail.com>
2023-08-04 20:30:33 +0300

    feat: add loaders for writing the styles and amend webpack config respectively

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,21 +1,32 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: "./src/index.ts",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
     ],
   },
   resolve: {
-    extensions: ['.ts'],
+    extensions: [".ts"],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
 };
