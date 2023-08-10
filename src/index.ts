a40d6241785614c3a9a825d2e4e31fe32428b158
Milana <milanamamedova52@mail.ru>
2023-08-10 23:49:28 +0300

    feat:create function for footer and header, clean index.html

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ import "./assets/styles/normalize.scss";
 import "./assets/styles/style.scss";
 import "./assets/styles/styleHeadFoot.scss";
 import "./assets/styles/styleMain.scss";
+import setHeaderAndFooter from "./pages/Main";
+
+setHeaderAndFooter();
