b7484a0a669294b2c53e15e31742be4277d56005
Kate-Shepel <kateborichevskaya@gmail.com>
2023-08-13 22:52:26 +0200

    feat: add toggle password visibility functionality

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,10 @@ import "./assets/styles/styleHeadFoot.scss";
 import "./assets/styles/styleMain.scss";
 import setBasicLayout from "./pages/BasicLayout";
 import initSameShippingListener from "./modules/setShippingAddressData";
+import togglePasswordVisibility from "./modules/togglePasswordVisibility";
 import { insertLoginFormIntoMain } from "./pages/Login";
 
 setBasicLayout();
 insertLoginFormIntoMain();
 initSameShippingListener();
+togglePasswordVisibility();
---
+++ src/modules/togglePasswordVisibility.ts
@@ -0,0 +1,17 @@
+export default function togglePasswordVisibility(): void {
+  const btnShowPass = document.querySelector(".btn-show-pass");
+  const icon = btnShowPass?.querySelector("i");
+  const passwordInput = document.querySelector("input[type='password']");
+
+  btnShowPass?.addEventListener("click", () => {
+    if (icon?.classList.contains("fa-eye-slash")) {
+      icon.classList.remove("fa-eye-slash");
+      icon.classList.add("fa-eye");
+      passwordInput?.setAttribute("type", "text");
+    } else {
+      icon?.classList.remove("fa-eye");
+      icon?.classList.add("fa-eye-slash");
+      passwordInput?.setAttribute("type", "password");
+    }
+  });
+}
