e147279df2bab9181892ad6f5ee65ac059c6325a
Milana <milanamamedova52@mail.ru>
2023-08-15 15:44:37 +0300

    feat:add events for footer

--- src/components/footerEvents.ts
+++ src/components/footerEvents.ts
@@ -3,19 +3,21 @@ const footerEvents = () => {
   navButton?.addEventListener("click", () => {
     const navMenu = document.querySelector(".footer-nav");
     navMenu?.classList.toggle("footer-active");
-    navMenu?.classList.toggle("footer-active-margin");
+    navButton.classList.toggle("active-but");
   });
 
   const infoButton = document.querySelector(".foot-title-info");
   infoButton?.addEventListener("click", () => {
     const infoList = document.querySelector(".footer-info-list");
     infoList?.classList.toggle("footer-active");
+    infoButton.classList.toggle("active-but");
   });
 
   const aboutButton = document.querySelector(".foot-title-aboutus");
   aboutButton?.addEventListener("click", () => {
     const aboutusList = document.querySelector(".footer-aboutus-list");
     aboutusList?.classList.toggle("footer-active");
+    aboutButton.classList.toggle("active-but");
   });
 };
 
