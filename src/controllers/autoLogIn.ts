808d8a29f968997ba6ad851a775d561cb76f4003
Pavel <pauledelweissontop@gmail.com>
2023-08-21 20:37:33 +0300

    feat: add removing  irrelevant user token

--- src/controllers/autoLogIn.ts
+++ src/controllers/autoLogIn.ts
@@ -1,5 +1,5 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"peerDependencies": true}] */
-import { getUserToken } from "../modules/common/useUserToken";
+import { getUserToken, removeUserToken } from "../modules/common/useUserToken";
 import autoSignIn from "../api/autoSignIn";
 import { ISignInResponseData } from "../interfaces/IUserProfileData";
 import { setUserProfileFullData } from "../models/userProfileData";
@@ -11,7 +11,9 @@ const autoLogIn = async () => {
     const signInResponseData: ISignInResponseData = await autoSignIn(userToken as string);
     const { userProfileStoreData } = signInResponseData;
 
-    if (userProfileStoreData) setUserProfileFullData(userProfileStoreData);
+    if (userProfileStoreData) {
+      setUserProfileFullData(userProfileStoreData);
+    } else removeUserToken();
   }
 };
 
