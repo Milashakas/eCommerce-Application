fd075dfe630ed1ecdeeb49b8a9f1756c3d7ca742
Pavel <pauledelweissontop@gmail.com>
2023-08-27 15:20:29 +0300

    refactor: refactor styles structure and remove repeated code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
-import "./assets/styles/normalize.scss";
-import "./assets/styles/style.scss";
-import "./assets/styles/styleHeadFoot.scss";
-import "./assets/styles/styleMain.scss";
-import "./assets/styles/404.scss";
-import "./assets/styles/popupNotification.scss";
-import "./assets/styles/catalog.scss";
-import "./assets/styles/test.scss";
+import "./assets/styles/index.scss";
 import headerOpenMenuFunctional from "./modules/headerEvents";
 import footerOpenLinksFunctional from "./modules/footerEvents";
 import setBasicLayout from "./modules/SetBasicLayout";
