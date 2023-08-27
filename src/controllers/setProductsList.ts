943c8530fcc9987b002e600da4845cf0d4c2398d
Pavel <pauledelweissontop@gmail.com>
2023-08-27 19:58:10 +0300

    feat: adjust function for setting products list in store

--- src/controllers/setProductsList.ts
+++ src/controllers/setProductsList.ts
@@ -1,12 +1,21 @@
 import { Product } from "@commercetools/platform-sdk";
 import getProductsList from "../api/getProductsList";
 import { setProductsListAction } from "../redux/actions";
+import IProductsListResponseData from "../interfaces/IProducts";
+import { navigateTo } from "../router";
 
 import store from "../redux/createStore";
 
 const setProductsList = async () => {
-  const productsList: Product[] = (await getProductsList()).productsListData?.results as Product[];
-  store.dispatch(setProductsListAction(productsList));
+  const productsListResponseData: IProductsListResponseData = await getProductsList();
+  const { statucCode } = productsListResponseData;
+
+  if (!statucCode || (statucCode && !(statucCode >= 200 && statucCode < 299))) {
+    navigateTo("/404");
+  } else {
+    const productsList: Product[] = productsListResponseData.productsListData?.results as Product[];
+    store.dispatch(setProductsListAction(productsList));
+  }
 };
 
 export default setProductsList;
