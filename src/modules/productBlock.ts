ee3cebcd1df941f286540b49b7ea4eb4ca5a6228
Milana <milanamamedova52@mail.ru>
2023-09-03 00:32:39 +0300

    feat: add discount

--- src/modules/productBlock.ts
+++ src/modules/productBlock.ts
@@ -1,20 +1,43 @@
-const product = (name: string, img: string | undefined, price: number, des: string, id: string) => {
-  const code = `
+const pr = (nam: string, im: string, pri: string, d: string, id: string, s: string | undefined) => {
+  let code;
+  if (s === undefined) {
+    code = `
   <a class="product-block" id="${id}" href="/catalog/${id}">
   <div class="product-block-image">
-  <img src="${img}" class="product-block-img">
+  <img src="${im}" class="product-block-img">
   <div class="product-block-busket-button">
   <p>Add to bag</p>
   </div>
   </div>
   <div class="product-description-block">
-  <p class="product-block-name">${name}</p>
-  <p class="product-block-description">${des}</p>
-  <p class="product-block-price">${price} EUR</p>
+  <p class="product-block-name">${nam}</p>
+  <p class="product-block-description">${d}</p>
+  <p class="product-block-price">${pri} EUR</p>
   </div>
   </a>
   `;
+  } else {
+    code = `
+    <a class="product-block" id="${id}" href="/catalog/${id}">
+    <div class="product-block-image">
+    <img src="${im}" class="product-block-img">
+    <div class="product-block-busket-button">
+    <p>Add to bag</p>
+    </div>
+    </div>
+    <div class="product-description-block">
+    <p class="product-block-name">${nam}</p>
+    <p class="product-block-description">${d}</p>
+    <div class="product-block-prices">
+    <p class="product-block-price product-block-price-withoutdis">${pri} EUR</p>
+    <p class="product-block-price product-block-price-dis">${s} EUR</p>
+    </div>
+    </div>
+    </a>
+    `;
+  }
+
   return code;
 };
 
-export default product;
+export default pr;
--- src/modules/setCatalog.ts
+++ src/modules/setCatalog.ts
@@ -11,18 +11,39 @@ const setCatalog = async () => {
     if (img) {
       firstImage = img[0].url;
     }
+    let mainImg = "";
+    if (firstImage !== undefined) {
+      mainImg = firstImage;
+    }
     const price = item.masterData.current.masterVariant.prices;
-    let currentPrice = 1;
+    let currentPrice: number | string = 2;
+    let mainPrice = "1";
+    let sale;
+    let mainSale: undefined | string;
     if (price) {
       currentPrice = (price[0].value.centAmount / 100) as number;
+      const prodPrice = currentPrice.toFixed(2);
+      mainPrice = prodPrice;
+      console.log(typeof prodPrice);
+      sale = price[0].discounted;
+    }
+
+    if (sale !== undefined) {
+      sale = (sale.value.centAmount / 100) as number;
+      const prodSale = sale.toFixed(2);
+      mainSale = prodSale;
     }
     const des = item.masterData.current.description;
-    let currentDescription = "";
+    let currentDes = "";
     if (des) {
-      currentDescription = des["en-US"] as string;
+      currentDes = des["en-US"] as string;
     }
     const { id } = item;
-    catalog.innerHTML += createProduct(name, firstImage, currentPrice, currentDescription, id);
+    if (sale !== undefined) {
+      catalog.innerHTML += createProduct(name, mainImg, mainPrice, currentDes, id, mainSale);
+    } else {
+      catalog.innerHTML += createProduct(name, mainImg, mainPrice, currentDes, id, mainSale);
+    }
   });
 };
 
