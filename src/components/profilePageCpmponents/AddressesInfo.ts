8c91190e894a9e29ad7220b15b8dc57f24ff53f2
Pavel <pauledelweissontop@gmail.com>
2023-09-04 22:20:12 +0300

    fix: fix display of country in profile info

--- src/components/profilePageCpmponents/AddressesInfo.ts
+++ src/components/profilePageCpmponents/AddressesInfo.ts
@@ -1,15 +1,26 @@
+/* eslint-disable max-len */
 import ProfileInfoField from "../common/ProfileInfoField";
 import { IAdressessInfo } from "../../interfaces/IProfile";
+
+import { countries } from "../../modules/setShippingAddressData";
+
+interface ICountryObj {
+  name: string;
+  code: string;
+}
 
 const AddressesInfo = (addressesData: IAdressessInfo) => {
   const billingAddress = addressesData.addresses[0];
   const shippingAddress = addressesData.addresses[1];
 
+  const billingCountry = countries.find((countryObj: ICountryObj) => countryObj.code === billingAddress.country);
+  const shippingCountry = countries.find((countryObj: ICountryObj) => countryObj.code === shippingAddress.country);
+
   const view = `
     <h2>Billing Address</h2>
 
     <div class="billing-address-field">
-      ${ProfileInfoField("Country", billingAddress.country)}
+      ${ProfileInfoField("Country", billingCountry?.name as string)}
       ${ProfileInfoField("City", billingAddress.city)}    
       ${ProfileInfoField("Street", billingAddress.streetName)}
     </div>
@@ -23,7 +34,7 @@ const AddressesInfo = (addressesData: IAdressessInfo) => {
     <h2>Shipping Address</h2>
 
     <div class="shipping-address-field">
-      ${ProfileInfoField("Country", shippingAddress.country)}
+      ${ProfileInfoField("Country", shippingCountry?.name as string)}
       ${ProfileInfoField("City", shippingAddress.city)}
       ${ProfileInfoField("Street", shippingAddress.streetName)}
     </div>
