04392ae9f2ad64f1445f3f8eeb3d34ada6b04730
Pavel <pauledelweissontop@gmail.com>
2023-09-04 22:27:32 +0300

    fix: fix a bug

--- src/modules/setProfilePopupMenuOptions.ts
+++ src/modules/setProfilePopupMenuOptions.ts
@@ -1,6 +1,7 @@
 import { removeUserToken } from "./common/useUserToken";
 import { logoutProfileAction } from "../redux/actions";
 import store from "../redux/createStore";
+import { navigateTo } from "../router";
 
 const setProfilePopupMenuOptions = () => {
   const exitBtn: HTMLSpanElement = document.querySelector(".popup-menu-exit") as HTMLSpanElement;
@@ -8,6 +9,7 @@ const setProfilePopupMenuOptions = () => {
   exitBtn.addEventListener("click", async () => {
     removeUserToken();
     store.dispatch(logoutProfileAction());
+    navigateTo("/");
   });
 };
 
