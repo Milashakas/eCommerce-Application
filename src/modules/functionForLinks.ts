b60f4f3c50e13079550dc50fc22d039419b20794
Milana <milanamamedova52@mail.ru>
2023-09-14 22:53:43 +0300

    feat: add rsschool

--- src/modules/functionForLinks.ts
+++ src/modules/functionForLinks.ts
@@ -4,6 +4,9 @@ const setListenerForLinks = () => {
   const main = document.querySelector("main") as HTMLElement;
   const links = main.querySelectorAll("a");
   links.forEach((item) => {
+    if (item.dataset.link) {
+      return;
+    }
     item.addEventListener("click", (e) => {
       e.preventDefault();
       const link = e.currentTarget as HTMLAnchorElement;
