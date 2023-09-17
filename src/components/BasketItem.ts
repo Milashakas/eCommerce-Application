7db21c126752db4cee82abaf088c54b675dbaca9
Kate-Shepel <kateborichevskaya@gmail.com>
2023-09-18 00:14:31 +0200

    feat: add basket item component + its styles

--- src/components/BasketItem.ts
+++ src/components/BasketItem.ts
@@ -2,8 +2,14 @@ import { ICartItemData } from "../interfaces/ICart";
 
 const BasketItem = (itemData: ICartItemData) => {
   console.log(itemData);
-
   const view = `
+  <div class="basket-item" id="${itemData.id}">
+    <img class="basket-item-img" src="${itemData.imageLink}"></img>
+    <span class="basket-item-name">${itemData.name}</span>
+    <span class="basket-item-price">${itemData.price} EUR</span>
+    <button type="submit" class="basket-remove-button">Remove from Cart</button>
+  </div>
+  <div class="basket-item-line"></div>
   `;
 
   return view;
