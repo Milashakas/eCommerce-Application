1284b9710677198fcf3dbceff50cfc6a26ece942
Kate-Shepel <kateborichevskaya@gmail.com>
2023-09-18 15:34:20 +0200

    feat: add promo code info on basket page

--- src/pages/Basket.ts
+++ src/pages/Basket.ts
@@ -4,9 +4,15 @@ import WelcomeSection from "../components/common/WelcomeSection";
 const basketPage = () => {
   const code = `
     ${WelcomeSection("Basket")}
-
+    <div class="promotion-info">Fantastic Offer For Our Clients During Autumn 2023! Get 10% OFF For Everything Using Our Special Discount Code: AUTUMNSALECODE</div>
     <div class="basket-item-list"></div>
+    <div class="coupon">
+      <input type="text" name="coupon_code" class="coupon-code" id="coupon_code" value="" placeholder="Add Your Discount Code">
+      <button type="submit" class="code-button" name="apply_coupon" value="Apply code">
+      Apply discount</button>
+    </div>
     <p class="basket-total-price"></p>
+
     `;
   return code;
 };
